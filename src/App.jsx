dab9e6436df7a6f8002627f2404dc25ba62409b3
E S <shivakumar.esha12@gmail.com>
2026-02-28 19:58:12 -0800

    intitial starting code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Vite + React</h1>
+        <p>Basic starter app scaffolded by assistant.</p>
+      </header>
+    </div>
+  )
+}
